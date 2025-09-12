14c35e9fbb369a4801a509a38008abbfb1c788cf
alexserranofernandez <alex.serrano@campus.xaloc.org>
2025-09-12 16:03:55 +0200

    Update prueba 1.py

--- prueba 1.py
+++ prueba 1.py
@@ -8,8 +8,9 @@ if var1=="Real Madrid" or var1=="Madrid":
 else:
     print("Que equipazo! Estoy de acuerdo contigo!")
 var2=input ("Y cual es para ti el mejor jugador de todos los tiempos?")
-if var1=="Messi" or var1=="Leo Messi" or var1=="Ronaldo" or var1=="Cristiano Ronaldo"
+if var2=="Messi" or var2=="Leo Messi" or var2=="Ronaldo" or var2=="Cristiano Ronaldo":
     print("No esta mal...")
 else:
     print("¡El verdadero GOAT del futbol!") or ("¡Tu si que sabes chaval!")
+
 
