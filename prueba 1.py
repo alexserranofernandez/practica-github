3584371f6510d05e9ead3cafe1f30d600093ea6e
alexserranofernandez <alex.serrano@campus.xaloc.org>
2025-09-12 15:52:33 +0200

    Add files via upload

---
+++ prueba 1.py
@@ -0,0 +1,9 @@
+
+
+print ("Hola me llamo Gilberto el Grande")
+var1=input ("¿Cual es tu equipo favorito?")
+
+if var1=="Real Madrid" or var1=="Madrid":
+    print("Ese equipo es una basura!")
+else:
+    print("Que equipazo! Estoy de acuerdo contigo!")
