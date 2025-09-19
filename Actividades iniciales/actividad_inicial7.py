52bd74fac95939b0d2531e9622cf904e2802b58a
alexserranofernandez <alex.serrano@campus.xaloc.org>
2025-09-19 15:47:52 +0200

    python

--- Actividades iniciales/actividad_inicial7.py
+++ Actividades iniciales/actividad_inicial7.py
@@ -4,7 +4,14 @@ total_suma=var1+var2
 total_resta=var1-var2
 total_multiplicación=var1*var2
 total_división=var1/var2
+total_división_2decimales=round(total_división, 2)
 total_divisiónentera=var1//var2
 total_módulo=var1%var2
 total_exponente=var1**var2
-print("")
+print("El resultado total de la suma es:" ,total_suma)
+print("El resultado total de la resta es:" ,total_resta)
+print("El resultado total de la multiplicación es:" ,total_multiplicación)
+print("El resultado total de la división es:" ,total_división_2decimales)
+print("El resultado total de la división entera es:" ,total_divisiónentera)
+print("El resultado total del módulo es:" ,total_módulo)
+print("El resultado total del exponente es:" ,total_exponente)
