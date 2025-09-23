1a6f43115a48c92d98c1b16d092961781c563602
alexserranofernandez <alex.serrano@campus.xaloc.org>
2025-09-23 10:24:32 +0200

    python

---
+++ Actividades iniciales/actividad_inicial11.py
@@ -0,0 +1,5 @@
+var1=int(input("Introduce el lado del cuadrado"))
+total_perimetro=var1*4
+total_área=var1*var1
+print("El perimetro del cuadrado es:", total_perimetro)
+print("El área del cuadrado es:", total_área)
