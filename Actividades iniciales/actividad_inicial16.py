e1f4b8f31d4188354b23af59c5093b9f2c5e19c4
alexserranofernandez <alex.serrano@campus.xaloc.org>
2025-09-26 15:40:52 +0200

    Create actividad_inicial16.py

---
+++ Actividades iniciales/actividad_inicial16.py
@@ -0,0 +1,8 @@
+#sqrt=raiz cuadrada
+var1=int(input("Introduce un número"))
+import math
+total_raiz_cuadradra=math.sqrt(var1)
+total_división=total_raiz_cuadradra//2
+total_raiz_cuadradra_redondeada=round(total_raiz_cuadradra,1)
+print("La raiz cuadrada es:", total_raiz_cuadradra_redondeada)
+print("La división de la raiz cuadrada es:", total_división)
