5ecc70f0671c0925464a0d2e7b5e74ea2de30f53
alexserranofernandez <alex.serrano@campus.xaloc.org>
2025-09-26 15:22:45 +0200

    python

--- Actividades iniciales/actividad_inicial14.py
+++ Actividades iniciales/actividad_inicial14.py
@@ -2,7 +2,9 @@
 var1=float(input("Introduce el diametro del círculo"))
 import math
 total_perimetro=math.pi*var1
+total_perimetro_redondeado=round(total_perimetro,1)
 radio=var1/2
 total_área=math.pi*radio**2
-print("El perimetro es:", total_perimetro)
-print("El área es:", total_área)
+total_área_redondeada=round(total_área,1)
+print("El perimetro es:", total_perimetro_redondeado)
+print("El área es:", total_área_redondeada)
