a985514659ecdc2e9c75d05561dd9fca3fdb1db0
alexserranofernandez <alex.serrano@campus.xaloc.org>
2025-09-26 16:00:35 +0200

    Create actividad_inicial17.py

---
+++ Actividades iniciales/actividad_inicial17.py
@@ -0,0 +1,12 @@
+#var1=peso
+#var2=altura
+print("Este programa calcula tu IMC")
+var1=float(input("Por favor, introduce tu peso en kilogramos"))
+var2=float(input("Ahora introduce tu altura en metros"))
+altura_cuadrado=var2**2
+imc=var1/altura_cuadrado
+imc_redondeado=round(imc,2)
+if imc_redondeado>25:
+    print ("Si tu peso es de",var1,"kilogramos, y tu altura es de",var2,"metros, tu imc es de",imc_redondeado".Tienes sobrepeso.")
+else:
+    print ("Si tu peso es de",var1,"kilogramos, y tu peso es de",var2,"metros, tu imc es de",imc_redondeado)
