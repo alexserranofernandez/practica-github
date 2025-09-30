2b37830991b4096601875e143f3642fe47a66561
alexserranofernandez <alex.serrano@campus.xaloc.org>
2025-09-30 10:02:30 +0200

    Update actividad_inicial17.py

--- Actividades iniciales/actividad_inicial17.py
+++ Actividades iniciales/actividad_inicial17.py
@@ -7,6 +7,6 @@ altura_cuadrado=var2**2
 imc=var1/altura_cuadrado
 imc_redondeado=round(imc,2)
 if imc_redondeado>25:
-    print ("Si tu peso es de",var1,"kilogramos, y tu altura es de",var2,"metros, tu imc es de",imc_redondeado".Tienes sobrepeso.")
+    print ("Si tu peso es de",var1,"kilogramos, y tu altura es de",var2,"metros, tu imc es de",imc_redondeado,".Tienes sobrepeso.")
 else:
     print ("Si tu peso es de",var1,"kilogramos, y tu peso es de",var2,"metros, tu imc es de",imc_redondeado)
