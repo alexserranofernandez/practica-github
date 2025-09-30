e319ec2676c0fb16011739e36a2a6ec42cc5a8df
alexserranofernandez <alex.serrano@campus.xaloc.org>
2025-09-30 10:29:00 +0200

    python

---
+++ pruebas/prueba_condicional1.py
@@ -0,0 +1,7 @@
+var1=float(input("Introduce un número"))
+var2=float(input("Introduce otro número"))
+
+if var1==var2:
+    print("son iguales")
+elif var1!=var2:
+    print("son diferentes")
---
+++ pruebas/prueba_condicional2.py
@@ -0,0 +1,14 @@
+var1=int(input("Introduce un plato del menú (1,2,3,4)"))
+if var1==1:
+    print("El plato que has escogido es la hamburguesa")
+else:
+    if var1==2:
+        print("El plato que has escogido es la pizza")
+    else:
+        if var1==3:
+            print("El plato que has escogido son las lentejas")
+        else:
+            if var1==4:
+                print("El plato que has escogido es el bocadillo de jamón")
+            else:
+                print("Ese plato no está en el menú")
