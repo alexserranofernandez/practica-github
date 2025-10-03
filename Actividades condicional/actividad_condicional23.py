ae6a07ed3a15df76c9f475275d8e26c796e9f809
alexserranofernandez <alex.serrano@campus.xaloc.org>
2025-10-03 15:39:30 +0200

    python

--- Actividades condicional/actividad_condicional23.py
+++ Actividades condicional/actividad_condicional23.py
@@ -2,9 +2,7 @@
 #notable (>=6.5 -<8.5), satisfactorio (<6.5 -5) o insuficiente (<5). Controla que la nota 
 #introducida esté entre 0 y 10. Utilizar elif sin operadores lógicos.
 var1=float(input("Introduce la nota que has sacado"))
-if var1>10 or var1<0:
-    print("La nota que has introducido no está entre 0 y 10")
-elif var1>=8.5:
+if 10>var1>=8.5:
     print(f"Has sacado un {var1} y es un excelente")
 elif 8.5>var1>=6.5:
     print(f"Has sacado un {var1} y es un notable")
@@ -12,3 +10,5 @@ elif 6.5>var1>=5:
     print(f"Ha sacado un {var1} y es un satisfactorio")
 elif var1<5:
     print(f"Has sacado un {var1} y es un suspenso")
+else:
+    print("La nota que has introducido no está entre 0 y 10")
---
+++ Actividades condicional/actividad_condicional25.py
@@ -0,0 +1,12 @@
+#Repite el programa número 23 pero en esta ocasión utilizando operadores lógicos.
+var1=float(input("Introduce la nota que has sacado"))
+if var1>10 or var1<0:
+    print("La nota que has introducido no está entre 0 y 10")
+elif var1>=8.5:
+    print(f"Has sacado un {var1} y es un excelente")
+elif 8.5>var1>=6.5:
+    print(f"Has sacado un {var1} y es un notable")
+elif 6.5>var1>=5:
+    print(f"Ha sacado un {var1} y es un satisfactorio")
+elif var1<5:
+    print(f"Has sacado un {var1} y es un suspenso")
