d5fa20a5c46a49abffdea80aef8fa5b9588e31ac
alexserranofernandez <alex.serrano@campus.xaloc.org>
2025-10-03 16:02:42 +0200

    python

---
+++ Actividades condicional/actividad_condicional26.py
@@ -0,0 +1,12 @@
+#Realiza un programa que, al introducir una letra por teclado, aparezca por pantalla si 
+#está o no en mayúscula. Utiliza dos IF’s que establezcan True o False a la condición.
+var1=input("Introduce una letra")
+if len(var1) == 1 and var1.isalpha:
+    if var1.isupper():
+        print("La letra introducida es mayuscula")
+    elif var1.islower():
+        print("La letra introducida es minuscula")
+    elif var1.isnumeric():
+        print("El valor introducido es un número")
+else:
+    print("El valor introducido no es ni númerico ni alfabético")
