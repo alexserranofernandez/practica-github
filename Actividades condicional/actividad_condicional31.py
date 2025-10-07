60bae7ab36c43a15308b53b496e1cd3ffd26164f
alexserranofernandez <alex.serrano@campus.xaloc.org>
2025-10-07 10:45:25 +0200

    python

---
+++ Actividades condicional/actividad_condicional31.py
@@ -0,0 +1,6 @@
+#Asigna a una variable de texto la siguiente frase: A quién madruga Dios ayuda. 
+#Comprueba si existen las siguientes palabras mostrando por pantalla la posición de su 
+#índice
+var_frase=("A quién madruga Dios ayuda")
+palabra=("Introduce una palabra: ")
+palabra1=("A")
