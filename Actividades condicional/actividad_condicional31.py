d7fcde1b3a2065ec490c4fbcdf057d34c9f27f8b
alexserranofernandez <alex.serrano@campus.xaloc.org>
2025-10-08 09:30:13 +0200

    python

--- Actividades condicional/actividad_condicional31.py
+++ Actividades condicional/actividad_condicional31.py
@@ -1,6 +1,16 @@
 #Asigna a una variable de texto la siguiente frase: A quién madruga Dios ayuda. 
 #Comprueba si existen las siguientes palabras mostrando por pantalla la posición de su 
 #índice
-var_frase=("A quién madruga Dios ayuda")
-palabra=("Introduce una palabra: ")
-palabra1=("A")
+
+var_frase="A quién madruga Dios ayuda"
+palabra=input("Introduce una palabra: ")
+palabra1="A"
+palabra2="quién"
+palabra3="madruga"
+palabra4="Dios"
+palabra5="ayuda"
+indice=var_frase.index(palabra)
+if palabra == palabra1 or palabra2 or palabra3 or palabra4 or palabra5:
+    print(f"La palabra está en la frase y está en el indice {indice}.")
+else:
+    print(f"La palabra no está en la frase")
