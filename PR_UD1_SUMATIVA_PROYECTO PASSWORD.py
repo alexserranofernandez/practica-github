9e7237f9d3ef82fe8d5b7e350c1db95edafc7de6
alexserranofernandez <alex.serrano@campus.xaloc.org>
2025-10-24 16:03:34 +0200

    python

--- PR_UD1_SUMATIVA_PROYECTO PASSWORD.py
+++ PR_UD1_SUMATIVA_PROYECTO PASSWORD.py
@@ -1,9 +1,8 @@
 var_password=input("Introduce una contraseña de entre 6 y 8 caracteres")
 caracteres=len(var_password)
-if not len(var_password)>6 or not len(var_password)<8:
+if not len(var_password)>=6 or not len(var_password)<=8:
     print(f"ERROR, la contraseña contiene {caracteres} caracteres, lo cual no es viable.")
 else:
-    #primer caracter
-    caracter0=int(var_password[0])
-    if caracter0<=1 and caracter0>=5:
-        print("ERROR en el carcter 1")
+    caracter0=(var_password[0])
+    if 1<=caracter0<=5:
+        print("ERROR en el caracter 1")
