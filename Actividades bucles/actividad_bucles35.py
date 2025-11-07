1b424a29d288a09a0c6b7b6fc89cf73e05f4d530
alexserranofernandez <alex.serrano@campus.xaloc.org>
2025-11-07 15:21:53 +0100

    python

---
+++ Actividades bucles/actividad_bucles35.py
@@ -0,0 +1,6 @@
+#Programa que al introducir un número por teclado permita mostrar ese número de veces tu 
+#nombre
+var_nombre=input("Introduce tu nombre:")
+var_repeticiones=int(input("Introduce el número de repeticiones:"))
+for i in range(var_repeticiones):
+    print(var_nombre)
