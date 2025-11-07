7953da9bd714ffdec9bbae1327fee5e8a6146992
alexserranofernandez <alex.serrano@campus.xaloc.org>
2025-11-07 15:52:53 +0100

    python

---
+++ Actividades bucles/actividad_bucles37.py
@@ -0,0 +1,9 @@
+#Programa que pregunte cuantas notas quiero introducir y para cada nota diga si estoy aprobado 
+#o suspendido.
+var_repeticiones=int(input("Introduce el número de notas que quieres introducir:"))
+for i in range(var_repeticiones):
+    var_nota=int(input("Introduce una nota:"))
+    if var_nota>=5:
+        print("Asignatura aprobada")
+    else:
+        print("Asignatura suspendida")
