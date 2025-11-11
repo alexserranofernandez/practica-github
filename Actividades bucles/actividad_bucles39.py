da8196f2a2974f8760c2f5158abe260c255aaf57
alexserranofernandez <alex.serrano@campus.xaloc.org>
2025-11-11 10:03:30 +0100

    python

--- Actividades bucles/actividad_bucles39.py
+++ Actividades bucles/actividad_bucles39.py
@@ -1,7 +1,17 @@
 #Programa que pida n números y que, tras introducir el último número, debe aparecer por 
 #pantalla el número total de positivos, negativos y número de 0.
 var_repeticiones=int(input("Introduce el número de números que quieres introducir:"))
+var_0=0
+var_positivos=0
+var_negativos=0
 for i in range(var_repeticiones):
     var_número=int(input("Introduce un número:"))
     if var_número>0:
-        p
+        var_positivos=var_positivos+1
+    elif var_número<0:
+        var_negativos=var_negativos+1
+    else:
+        var_0=var_0+1
+print(f"El número de positivos es {var_positivos}")
+print(f"El número de negativos es {var_negativos}")
+print(f"El número de ceros es {var_0}")
