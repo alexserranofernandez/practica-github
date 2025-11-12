157313831d832c1294e608d19efb87db3aa05e73
alexserranofernandez <alex.serrano@campus.xaloc.org>
2025-11-12 09:29:23 +0100

    python

--- Actividades bucles/actividad_bucles44.py
+++ Actividades bucles/actividad_bucles44.py
@@ -1,8 +1,7 @@
 #Realiza un programa que recorra todos los números comprendidos de 0 a 100 realizando saltos 
 #de 3 en 3. El resultado debe aparecer por pantalla en una línea con los números separados por ‘,’
 var_numero=0
-var_indice=0
 for i in range(0,33):
-    var_indice=var_indice+1
-    var_numero=str(var_numero)+","+str(var_indice*3)
+    i=i+1
+    var_numero=str(var_numero)+","+str(i*3)
 print(f"{var_numero}")
--- Actividades bucles/actividad_bucles46.py
+++ Actividades bucles/actividad_bucles46.py
@@ -1,9 +1,10 @@
-#Realiza un programa que permita introducir una palabra por teclado y puedas recorrer el string
-#distinguiendo vocales y las consonantes:
+#A partir del programa anterior, soluciona el error que se produce en el test anterior con la 
+#palabra Abrigo utilizando únicamente una instrucción.
 vocal_palabra=""
 var_vocales="aeiouáéíóú"
 consonante_palabra=""
 var_palabra=input("Introduce una palabra:")
+var_palabra=var_palabra.lower()
 for i in range(len(var_palabra)):
     if var_palabra[i] in var_vocales:
         vocal_palabra=str(vocal_palabra)+str(var_palabra[i])
@@ -11,4 +12,3 @@ for i in range(len(var_palabra)):
         consonante_palabra=str(consonante_palabra)+str(var_palabra[i])
 print(f"Las vocales de la palabra {var_palabra} son: {vocal_palabra}")
 print(f"La consonantes de la palabra {var_palabra} son: {consonante_palabra}")
-        
