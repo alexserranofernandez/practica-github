daa07a7be84fb42ccb9d6c75e2a8563b81d5b453
alexserranofernandez <alex.serrano@campus.xaloc.org>
2025-11-14 16:00:47 +0100

    python

--- Actividades bucles/actividad_bucles49.py
+++ Actividades bucles/actividad_bucles49.py
@@ -1,17 +1,17 @@
 #A partir del programa anterior, modifica el código para que al introducir la letra por teclado te 
 #indique en qué posición de la palabra se encuentra la letra.
-var_palabra=input("Introduce una palabra:")
-var_palabra=var_palabra.lower
+palabra=input("Introduce una palabra:")
+palabra=palabra.lower()
 var_oportunidades=5
-var_longitud=len(var_palabra)
 for i in range(var_oportunidades):
-    var_letra=input("Introduce una letra:")
-    if var_letra not in var_palabra:
-        print("La letra no está en la palabra")
+    for j in range (0,len(palabra)):
+        var_letra=input("Introduce una letra:")
+        index=palabra.index(var_letra)+1
+        if var_letra not in palabra:
+            print("La letra no está en la palabra")
         var_oportunidades=var_oportunidades-1
-    if var_oportunidades<=0:
-        print("Te has quedado sin oportunidades")
-        
-    else:   
-        print(f"La letra está en la posición  en la palabra")
-        exit()
+        if var_oportunidades<=0:
+            print("Te has quedado sin oportunidades")
+        else:   
+            print(f"La letra está en la posición {index} en la palabra y te han sobrado {var_oportunidades+1} oportunidades.")
+exit()
