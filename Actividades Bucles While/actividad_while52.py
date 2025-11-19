98f5a1bf1406770b40374824acab2953212d3ad5
alexserranofernandez <alex.serrano@campus.xaloc.org>
2025-11-19 09:34:05 +0100

    python

--- Actividades Bucles While/actividad_while52.py
+++ Actividades Bucles While/actividad_while52.py
@@ -1,11 +1,7 @@
 #Realiza un programa que sume dos números enteros por teclado y presente el resultado por 
 #pantalla. El programa preguntará si deseas o no repetir la operación. Con While
-var_numero1=int(input("Introduce un número:"))
-var_numero2=int(input("Introduce otro número:"))
-var_suma=var_numero1+var_numero2
-print(f"El resultado de la suma es {var_suma}")
-repetición=input("Desea repetir la operación s/n")
-while repetición!="n":
+repetición="s"
+while repetición=="s":
     var_numero1=int(input("Introduce un número:"))
     var_numero2=int(input("Introduce otro número:"))
     var_suma=var_numero1+var_numero2
---
+++ Actividades Bucles While/actividad_while53.py
@@ -0,0 +1,17 @@
+#A partir del código anterior, haz que aparezca al finalizar el programa por pantalla el total las 
+#sumas y el número de repeticiones. Con While
+suma_total=0
+repeticiones_totales=0
+repetición="s"
+while repetición=="s":
+    var_numero1=int(input("Introduce un número:"))
+    var_numero2=int(input("Introduce otro número:"))
+    var_suma=var_numero1+var_numero2
+    print(f"El resultado de la suma es {var_suma}")
+    repeticiones_totales=repeticiones_totales+1
+    suma_total=suma_total+var_suma
+    repetición=input("Desea repetir la operación s/n:")
+else:
+    print("Fin del programa. Resumen")
+    print(f"La suma total es: {suma_total} y el número de repeticiones es: {repeticiones_totales} ")
+    
