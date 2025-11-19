2dd6ddd421ad0a321fd84cdc810b1f7926de6c6c
alexserranofernandez <alex.serrano@campus.xaloc.org>
2025-11-19 09:11:55 +0100

    python

---
+++ Actividades Bucles While/actividad_while1.py
@@ -0,0 +1,7 @@
+#Realiza un programa que de los buenos días 3 veces. Con While
+repeticiones=3
+while repeticiones!=0:
+    print("buenos dias")
+    repeticiones=repeticiones-1
+    if repeticiones<=0:
+        break
--- pruebas/prueba_bucles_while1,2.py
+++ pruebas/prueba_bucles_while1,2.py
@@ -2,6 +2,8 @@ intentos=3
 import random
 numero_secreto=random.randint(1,20)
 numero_introducido=int(input("Introduce un número del 1 al 20:"))
+if intentos<=0:
+    print(f"Te has quedado sin intentos, el número secreto era {numero_secreto}")
 while intentos!=0:
     while numero_introducido<numero_secreto:
         print("Es más alto")
@@ -13,7 +15,6 @@ while intentos!=0:
         intentos=intentos-1
         print(f"Te quedan {intentos} intentos")
         numero_introducido=int(input("Introduce otro número:"))
-    print(f"Acertaste, te han sobrado {intentos-1} intentos")
-    break
-if intentos==0:
-    print(f"Te has quedado sin intentos, el número secreto era {numero_secreto}")
+        print(f"Acertaste, te han sobrado {intentos-1} intentos")
+        break
+
