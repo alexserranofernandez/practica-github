7c3ba99c06fe6e79376b17c845c53536ba1eae80
alexserranofernandez <alex.serrano@campus.xaloc.org>
2025-11-19 09:16:08 +0100

    python

---
+++ Actividades Bucles While/actividad_while51.py
@@ -0,0 +1,6 @@
+#A partir del programa anterior, modifica el código para que sea el usuario quién introduzca el 
+#número de veces que desea que repita la frase Buenos días. Con While
+repeticiones=int(input("Introduce el número de veces que quieres que se repita la palabra:"))
+while repeticiones!=0:
+    print("buenos dias")
+    repeticiones=repeticiones-1
