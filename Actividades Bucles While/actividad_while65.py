533da356da7ca5451d70c0c6a51e1f43063ccebb
alexserranofernandez <alex.serrano@campus.xaloc.org>
2025-12-03 09:45:57 +0100

    python

---
+++ Actividades Bucles While/actividad_while65.py
@@ -0,0 +1,2 @@
+#Programa que pida continuamente números por teclado hasta que el usuario introduzca el valor 
+#-99. Por pantalla debe aparecer cuál de todos los números introducidos es el mayo y cuál el menor.
