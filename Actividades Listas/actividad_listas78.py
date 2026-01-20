64ab867db9be162bbd8b133c476b2392b97a60ec
alexserranofernandez <alex.serrano@campus.xaloc.org>
2026-01-20 10:44:34 +0100

    python

---
+++ Actividades Listas/actividad_listas78.py
@@ -0,0 +1,16 @@
+#A partir de la lista definida en el ejercicio 75, haz que el programa pregunte qué valor se desea 
+#eliminar de la lista, siendo únicamente los números los valores permitidos para suprimir
+lista1=["a","b","D","x","r","X","3","h","w","2","i"]
+print(lista1)
+repeat="s"
+eliminado=""
+while repeat=="s":
+    eliminado=input("Introduce el valor que deseas eliminar:")
+    if eliminado in lista1:
+        if eliminado.isalpha():
+            print("Introducir valor númerico")
+            repeat=("¿Deseas introducir otro valor? s/n")
+        elif eliminado.isnumeric():
+            lista1=lista1.remove(eliminado)
+            print(lista1)
+    else:
