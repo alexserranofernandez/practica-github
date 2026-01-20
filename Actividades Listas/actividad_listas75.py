5e4ce716e6537ebe4e56c5bce63b4424e4dd6781
alexserranofernandez <alex.serrano@campus.xaloc.org>
2026-01-20 10:13:59 +0100

    python

--- Actividades Listas/actividad_listas75.py
+++ Actividades Listas/actividad_listas75.py
@@ -8,6 +8,20 @@
 lista1=["a","b","D","x","r","X","3","h","w","2","i"]
 cantidad_valores=len(lista1)
 numeros=0
+letras=0
+mayuscula=0
+suma=0
 for i in lista1: 
     if i.isnumeric():
         numeros=numeros+1
+        suma=suma+int(i)
+    if i.isalpha():
+        letras=letras+1
+        if i.isupper():
+            mayuscula=mayuscula+1
+print(f"Número de valores: {cantidad_valores}")
+print(f"Cantidad de números: {numeros}")
+print(f"Cantidad de letras: {letras}")
+print(f"Cantidad de mayusculas: {mayuscula}")
+print(f"Suma total de números: {suma}")
+    
