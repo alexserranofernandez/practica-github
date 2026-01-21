283aa89ed3305fa14c95593fa7ede6833c02df88
alexserranofernandez <alex.serrano@campus.xaloc.org>
2026-01-21 09:48:25 +0100

    python

---
+++ Actividades Listas/actividad_listas81.py
@@ -0,0 +1,6 @@
+#A partir de una lista definida, busca el método apropiado para que se visualice un valor de la 
+#lista al azar.
+lista=["casa","barco","gato","perro","madera","agua","puente","pantalón"]
+import random
+palabra=random.choice(lista)
+print(palabra)
